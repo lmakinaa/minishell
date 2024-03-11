46f49334f5e86b4b50115b3758dd6425385fca8e
Lmakinaa <jaija.ismail@gmail.com>
2024-03-11 12:30:19 +0000

    hmm

---
+++ allocation_manager/allocation_manager.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   allocation_manager.h                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ijaija <ijaija@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/11 11:27:54 by ijaija            #+#    #+#             */
+/*   Updated: 2024/03/11 12:23:36 by ijaija           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ALLOCATION_MANAGER_H
+# define ALLOCATION_MANAGER_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+typedef struct s_heapaddr
+{
+	void				*addr;
+	struct s_heapaddr	*next;
+}			t_memslot;
+
+typedef struct s_heapslots
+{
+	struct s_heapaddr	*first;
+	struct s_heapaddr	*last;
+	int					count;
+}			t_memsession;
+
+void	exit_on_alloc_error(void);
+void	session_init(t_memsession **session_ptr);
+void	*ultra_malloc(t_memsession *session, size_t size);
+void	session_destroy(t_memsession **session_ptr);
+
+#endif
---
+++ allocation_manager/ultra_malloc.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ultra_malloc.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ijaija <ijaija@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/11 11:33:17 by ijaija            #+#    #+#             */
+/*   Updated: 2024/03/11 12:23:36 by ijaija           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "allocation_manager.h"
+
+void	*ultra_malloc(t_memsession *session, size_t size)
+{
+	t_memslot	*new_slot;
+
+	new_slot = malloc(sizeof(t_memslot));
+	if (!new_slot)
+		exit_on_alloc_error();
+	new_slot->addr = malloc(size);
+	if (!new_slot->addr)
+		exit_on_alloc_error();
+	new_slot->next = NULL;
+	if (!session->first)
+	{
+		session->first = new_slot;
+		session->last = new_slot;
+	}
+	else
+	{
+		session->last->next = new_slot;
+		session->last = new_slot;
+	}
+	session->count++;
+	return (new_slot->addr);
+}

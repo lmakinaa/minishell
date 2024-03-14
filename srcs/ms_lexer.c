2ab0f4bd55416d7edaa0439cad0f7a10b4be7afa
Lmakinaa <jaija.ismail@gmail.com>
2024-03-14 16:05:11 +0000

    hmm

--- srcs/ms_lexer.c
+++ srcs/ms_lexer.c
@@ -6,13 +6,25 @@
 /*   By: ijaija <ijaija@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 12:04:32 by ijaija            #+#    #+#             */
-/*   Updated: 2024/03/14 13:57:39 by ijaija           ###   ########.fr       */
+/*   Updated: 2024/03/14 15:54:11 by ijaija           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./../includes/minishell.h"
 
-t_token	**ms_lexer(t_memsession *session, char *command)
+t_token	*ms_lexer(t_memsession *session, char *command)
 {
+	t_token		*res;
+	t_splitdata	*splited_cmd;
+	int			i;
+
+	splited_cmd = advanced_split(session, command, SEPERATORS);
+	res = session_malloc(session, splited_cmd->word_count * sizeof(t_token));
+	i = -1;
+	while (++i < splited_cmd->word_count)
+	{
+		res[i].data = splited_cmd->words[i];
+		
+	}
 	
 }

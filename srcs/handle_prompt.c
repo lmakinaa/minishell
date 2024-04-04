2d51f753b90fc53d51b1b978360ce8bf778f7580
Ismail Jaija <ijaija@e1r4p5.1337.ma>
2024-04-04 16:58:27 +0000

    hmm

--- srcs/handle_prompt.c
+++ srcs/handle_prompt.c
@@ -36,33 +36,11 @@ int	check_if_parenthesis_closed(char *s)
 	return (0);
 }
 
-int	check_if_quotes_closed(char *s)
-{
-	int	i;
-	int	s_quotes;
-	int	d_quotes;
-
-	i = 0;
-	s_quotes = 0;
-	d_quotes = 0;
-	while (s[i])
-	{
-		if (s[i] == '"')
-			d_quotes++;
-		else if (s[i] == '\'')
-			s_quotes++;
-		i++;
-	}
-	if (s_quotes % 2 || d_quotes % 2)
-		return (write(2, "Syntax error: related to quotes\n", 32), 1);
-	return (0);
-}
-
 void	handle_prompt(t_memsession *heap_session, char *prompt)
 {
 	t_token	*tokens; // array of tokens
 
-	if (check_if_parenthesis_closed(prompt) || check_if_quotes_closed(prompt))
+	if (check_if_parenthesis_closed(prompt))
 		return ;
 	tokens = ms_lexer(heap_session, prompt);
 	// handle_commands();

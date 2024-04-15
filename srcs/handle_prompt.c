43fcdbb6712593aa61e8adcdf02a55580b99adef
Lmakinaa <jaija.ismail@gmail.com>
2024-04-15 21:42:13 +0100

    hmm

--- srcs/handle_prompt.c
+++ srcs/handle_prompt.c
@@ -44,8 +44,10 @@ void	handle_prompt(t_memsession *heap_session, char *prompt)
 	if (check_if_parenthesis_closed(prompt))
 		return ;
 	tokens = ms_lexer(heap_session, prompt);
-	// print_tokens(tokens);
+	// syntax_analyze_1()
 	root = build_tree(heap_session, &tokens, 0);
 	print_tree(root, 0);
-	// handle_commands();
+	// expander()
+	// here-doc()
+	// execute()
 }

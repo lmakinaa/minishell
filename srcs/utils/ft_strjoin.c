d0a373b2b2d95f53d3563e8de57c08258ae0b663
Lmakinaa <jaija.ismail@gmail.com>
2024-04-21 21:44:39 +0100

    hmm

--- srcs/utils/ft_strjoin.c
+++ srcs/utils/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: ijaija <ijaija@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/21 14:28:10 by ijaija            #+#    #+#             */
-/*   Updated: 2024/04/21 15:55:39 by ijaija           ###   ########.fr       */
+/*   Updated: 2024/04/21 21:44:31 by ijaija           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,30 +23,33 @@ char	*ft_strjoin(t_memsession *session, char *s1, char *s2)
 		return (s1);
 	size = ft_strlen(s1) + ft_strlen(s2);
 	res = session_malloc(session, (size + 1) * sizeof(char), 0);
-	i = 0;
-	j = 0;
-	while (i < size && s1[i])
-	{
+	i = -1;
+	while (++i < size && s1[i])
 		res[i] = s1[i];
-		i++;
-	}
-	while (i + j < size && s2[j])
+	j = 0;
+	while (i + j < size && *s2)
 	{
-		res[i + j] = s2[j];
-		j++;
+		if (*s2 != '"')
+		{
+			res[i + j] = *s2;
+			j++;
+		}
+		s2++;
 	}
 	res[i + j] = '\0';
 	del_from_session(session, s1);
 	return (res);
 }
 
-char	*ft_charjoin(t_memsession *session, char *s1, char c)
+char	*ft_joinchar(t_memsession *session, char *s1, char c)
 {
 	size_t	size;
 	char	*res;
 	size_t	i;
 	size_t	j;
 
+	if (c == '"')
+		return (s1);
 	size = ft_strlen(s1) + 1;
 	res = session_malloc(session, (size + 1) * sizeof(char), 0);
 	i = 0;

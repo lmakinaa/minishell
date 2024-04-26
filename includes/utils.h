c9e9ab69921dbf460ac1a31bc3445a23a4a0d95f
Ismail Jaija <ijaija@e2r11p5.1337.ma>
2024-04-26 16:32:13 +0100

    hmm

--- includes/utils.h
+++ includes/utils.h
@@ -6,7 +6,7 @@
 /*   By: ijaija <ijaija@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 13:41:27 by ijaija            #+#    #+#             */
-/*   Updated: 2024/04/26 13:19:17 by ijaija           ###   ########.fr       */
+/*   Updated: 2024/04/26 16:28:37 by ijaija           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,6 +46,7 @@ char		*get_next_line(t_memsession *session, int fd);
 char		*ft_strdup(t_memsession *session, char *s1, size_t len);
 char		*ft_strjoin(t_memsession *session, char *s1, char *s2);
 char		*ft_joinchar(t_memsession *session, char *s1, char c);
+int			ft_countword(char *str, char *seps);
 t_splitdata	*ft_split(t_memsession *session, char *s, char *seps);
 
 #endif

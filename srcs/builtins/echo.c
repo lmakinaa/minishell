58653e18820f984e5ed10a5ebae86f467e2a7094
Ismail Jaija <ijaija@e1r5p11.1337.ma>
2024-04-28 14:43:22 +0100

    hmm

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -6,7 +6,47 @@
 /*   By: ijaija <ijaija@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 22:18:50 by ijaija            #+#    #+#             */
-/*   Updated: 2024/04/26 22:18:51 by ijaija           ###   ########.fr       */
+/*   Updated: 2024/04/28 14:38:19 by ijaija           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+static int	check_option(char *s)
+{
+	int	i;
+
+	i = 0;
+	if (s[0] != '-')
+		return (0);
+	i++;
+	while (s[i])
+	{
+		if (s[i] != 'n')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	b_echo(char **args)
+{
+	int	i;
+	int	option;
+
+	i = 1;
+	option = 0;
+	while (args[i] && check_option(args[i]) == 1)
+	{
+		option = 1;
+		i++;
+	}
+	while (args[i])
+	{
+		write(1, args[i], ft_strlen(args[i]));
+		if (args[i + 1])
+			write(1, " ", 1);
+		i++;
+	}
+	if (option == 0)
+		write(1, "\n", 1);
+	return (0);
+}

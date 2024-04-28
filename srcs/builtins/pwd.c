25a16a58dd1552d432d1826e200cc36852cebff8
Lmakinaa <jaija.ismail@gmail.com>
2024-04-28 13:43:01 +0100

    hmm

--- srcs/builtins/pwd.c
+++ srcs/builtins/pwd.c
@@ -6,7 +6,24 @@
 /*   By: ijaija <ijaija@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 22:19:01 by ijaija            #+#    #+#             */
-/*   Updated: 2024/04/26 22:19:02 by ijaija           ###   ########.fr       */
+/*   Updated: 2024/04/27 00:32:04 by ijaija           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "./../../includes/minishell.h"
+
+int	pwd(void)
+{
+	char	*cwd;
+
+	cwd = NULL;
+	cwd = getcwd(cwd, 0);
+	if (!cwd)
+		exit_on_error("getcwd() failed", 15);
+	if (write(1, cwd, ft_strlen(cwd)) == -1)
+		return (free(cwd), -1);
+	if (write(1, "\n", 1) == -1)
+		return (free(cwd), -1);
+	free(cwd);
+	return (0);
+}

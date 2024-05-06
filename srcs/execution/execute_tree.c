4d2390d2c7926558d9525226134eae74cf1c5df1
Ismail Jaija <ijaija@e1r4p11.1337.ma>
2024-05-06 15:23:55 +0100

    hmm

--- srcs/execution/execute_tree.c
+++ srcs/execution/execute_tree.c
@@ -6,7 +6,7 @@
 /*   By: ijaija <ijaija@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 12:26:14 by ijaija            #+#    #+#             */
-/*   Updated: 2024/05/05 22:09:20 by ijaija           ###   ########.fr       */
+/*   Updated: 2024/05/06 15:23:44 by ijaija           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,10 +34,6 @@ int	execute_tree(t_memsession *session, t_lenv *env, t_tnode *root)
 	}
 	return (0);
 }
-// ""*""
+
 // $""PWD
 // $''PWD
-// "$""PWD"
-// "$""PWD"
-// any""''
-// "$"PWD

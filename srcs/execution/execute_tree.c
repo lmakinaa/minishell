757484cf416fc7cfb3134c03ef5b7a6d57f3e894
Lmakinaa <jaija.ismail@gmail.com>
2024-05-13 10:39:27 +0100

    hmm

--- srcs/execution/execute_tree.c
+++ srcs/execution/execute_tree.c
@@ -6,32 +6,11 @@
 /*   By: ijaija <ijaija@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 12:26:14 by ijaija            #+#    #+#             */
-/*   Updated: 2024/05/13 01:20:49 by ijaija           ###   ########.fr       */
+/*   Updated: 2024/05/13 10:39:24 by ijaija           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./../../includes/minishell.h"
-
-//int	execute_tree(t_memsession *session, t_lenv *env, t_tnode *root)
-//{
-//	t_command	*command;
-
-//	if (!root)
-//		return (0);
-//	if (execute_tree(session, env, root->left) == -1)
-//		return (-1);
-//	if (execute_tree(session, env, root->right) == -1)
-//		return (-1);
-//	if (root->command)
-//	{
-//		command = expand_n_generate_cmd(session, env, root->command);
-//		if (!command)
-//			return (-1);
-//		execute_command(command);
-//		//print_command_elements(command);
-//	}
-//	return (0);
-//}
 
 int	execute_tree(t_memsession *session, t_lenv *env, t_tnode *root, int pip)
 {

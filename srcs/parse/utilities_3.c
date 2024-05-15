f81d21825a645fbb41b6cfdfafd9af4caa89b37f
Ismail Jaija <ijaija@e1r1p5.1337.ma>
2024-05-15 18:40:39 +0100

    hmm

--- srcs/parse/utilities_3.c
+++ srcs/parse/utilities_3.c
@@ -6,7 +6,7 @@
 /*   By: ijaija <ijaija@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 16:52:57 by ijaija            #+#    #+#             */
-/*   Updated: 2024/05/15 11:08:41 by ijaija           ###   ########.fr       */
+/*   Updated: 2024/05/15 18:40:38 by ijaija           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,6 +66,7 @@ t_lenv	*ft_initialise(t_memsession **session1, t_memsession **session2,
 	session_init(session2);
 	env = envs_init(*session2, envp);
 	(*session1)->envs = env;
+	//signal(SIGINT, );
 	return (env);
 }
 

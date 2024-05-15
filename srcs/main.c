387ac284ea125a4dcfb823d7ec617d52488ce45e
Lmakinaa <jaija.ismail@gmail.com>
2024-05-15 15:05:25 +0100

    hmm

--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,31 @@
 /*   By: ijaija <ijaija@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 20:46:03 by ijaija            #+#    #+#             */
-/*   Updated: 2024/05/13 01:49:53 by ijaija           ###   ########.fr       */
+/*   Updated: 2024/05/15 13:30:24 by ijaija           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./../includes/minishell.h"
+
+char	*generate_prompt(t_memsession *session, t_lenv *env)
+{
+	char	*buffer;
+	char	*tmp;
+	char	*cwd;
+
+	cwd = get_env(env, "PWD");
+	tmp = ft_strnstr(cwd, get_env(env, "HOME"), ft_strlen(cwd));
+	buffer = ft_strdup(session, "\e[0;33m", 7);
+	if (tmp)
+	{
+		buffer = ft_joinchar(session, buffer, '~');
+		buffer = ft_strjoin(session, buffer, tmp);
+	}
+	else
+		buffer = ft_strjoin(session, buffer, cwd);
+	buffer = ft_strjoin(session, buffer, " \e[1;30m> \033[0m");
+	return (buffer);
+}
 
 void	f()
 {
@@ -40,7 +60,7 @@ int	main(int argc, char **argv, char **envp)
 	//envs_display(env);
 	while (1)
 	{
-		command = readline(PROMPT_TEXT);
+		command = readline(generate_prompt(main_session, env));
 		if (!command)
 			break ;
 		add_to_session(main_session, command);
@@ -52,3 +72,6 @@ int	main(int argc, char **argv, char **envp)
 	end_sessions(&main_session, &env_session);
 	return (0);
 }
+
+//export A="   ls   "
+//export B=$A

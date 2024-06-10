365c89a1bcc957e48db73e798be23058a4fe5464
lmakinaa <ijaija@e2r9p2.1337.ma>
2024-06-10 16:44:47 +0100

    hmm

--- srcs/builtins/env.c
+++ srcs/builtins/env.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   env.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mtalbi <mtalbi@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ijaija <ijaija@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/05 11:09:40 by mtalbi            #+#    #+#             */
-/*   Updated: 2024/06/08 18:06:19 by mtalbi           ###   ########.fr       */
+/*   Updated: 2024/06/10 16:40:37 by ijaija           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ int	b_env(t_command *command)
 {
 	t_env	*head;
 
+	if (command->argc != 1)
+		return (throw_error("env accepts no argument", 0, 23, 0), 1);
 	head = command->env->head;
 	while (head)
 	{
